14ac4034dcd59a14bae03f219f898f8cda17a260
Marcelo <marcelo.leite@mercadolivre.com>
2024-01-14 19:11:55 -0300

    feat: first commit

---
+++ src/main/java/com/grupo29/techflix/TechflixApplication.java
@@ -0,0 +1,13 @@
+package com.grupo29.techflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechflixApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TechflixApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/grupo29/techflix/TechflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo29.techflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechflixApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
